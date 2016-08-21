583e5c88c534ff634f318f0ded611cb48ac5a37b
Marcos Medina <marcos@MacBook-Pro-de-Marcos.local>
2016-08-20 20:52:28 -0500

    Initial Commit

---
+++ Time2DrinkTests/Time2DrinkTests.swift
@@ -0,0 +1,36 @@
+//
+//  Time2DrinkTests.swift
+//  Time2DrinkTests
+//
+//  Created by Marcos Ipanaque Medina on 20/08/16.
+//  Copyright © 2016 Marcos Ipanaque Medina. All rights reserved.
+//
+
+import XCTest
+@testable import Time2Drink
+
+class Time2DrinkTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
